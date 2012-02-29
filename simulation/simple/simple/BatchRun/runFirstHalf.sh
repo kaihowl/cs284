cbc63932d39f177c6887bad6e53e73d8c00fadcc
kaih <kai+github@hoewelmeyer.eu>
2012-02-29 11:50:52 -0800

    Deactivate beaconing, smaller battery

---
+++ simulation/simple/simple/BatchRun/runFirstHalf.sh
@@ -0,0 +1,12 @@
+echo Running exp1
+cd Experiment-1
+qualnet Experiment-1.config 
+cd ..
+echo Running exp2
+cd Experiment-2
+qualnet Experiment-2.config 
+cd ..
+echo Running exp3
+cd Experiment-3
+qualnet Experiment-3.config 
+cd ..
