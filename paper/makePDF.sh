fc5e9940da22fec1ee9f4ea0e9e0df59de31eb98
Faisal Nawab <faisalsyn@gmail.com>
2012-03-16 15:43:33 -0700

    modified splitting errors

--- paper/makePDF.sh
+++ paper/makePDF.sh
@@ -1,6 +1,6 @@
-latex *.tex 
+latex sleeping.tex 
 bibtex *.aux
-latex *.tex
-latex *.tex
-dvipdf *.dvi
+latex sleeping.tex
+latex sleeping.tex
+dvipdf sleeping.dvi
 #latex TMAC-camera-ready-8May.tex; dvips -o TMAC-camera-ready-8May.ps TMAC-camera-ready-8May.dvi; ps2pdf TMAC-camera-ready-8May.ps
